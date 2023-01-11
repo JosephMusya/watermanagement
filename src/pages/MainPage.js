4fa8ff48e4640be0be330d86bcbb9783ad9e855b
Joseph Musya <josephmusya254@gmail.com>
2023-01-11 10:17:33 +0300

    Imported the tank and displayed it

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,20 +1,31 @@
 import styles from './TankStyle.module.css';
 function MainPage(){
     return (
-    <div id="tank" className={styles.tank}>
-        <div id="water-level" className="water-level"></div>
-            <div class="wave">
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
+        <div className={styles.mainPage}>
+            <div>
+                <div id="tank" className={styles.tank}>
+                <div id="water-level" className={styles.waterlevel}></div>
+                    <div className={styles.wave}>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                        <div className={styles.box}></div>
+                    </div>
+                </div>
+                <div className={styles.controls}>
+                    <strong>Quantity:&nbsp;<span>30 Litres</span></strong>
+                    <button className="btn text-white bg-primary">Close</button>
+                    <button className='btn text-white bg-danger'>Open</button>
+                </div>
+                hhh
+            </div>                  
         </div>
-    </div>
+    
     );
 }
 
