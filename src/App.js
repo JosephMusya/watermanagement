beefda2cb1f022ac5a1c6b95ead00ccb5be25390
Joseph Musya <josephmusya254@gmail.com>
2023-01-11 12:10:04 +0300

    Added Link for pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./components/MainNav";
 import MainPage from "./pages/MainPage";
 import TankSetup from "./pages/Setup";
+import Alerts from "./pages/Alerts";
+import Profile from "./pages/Profile";
 import {Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' exact element={<MainPage/>}/>
         <Route path='/tank-setup' element={<TankSetup/>}/>
+        <Route path='/alerts' element={<Alerts/>}/>
+        <Route path='/user-profile' element={<Profile/>}/>
       </Routes>      
     </div>
   );
