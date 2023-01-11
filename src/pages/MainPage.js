800d0b76eca5867fbb913fc5ce762a25da5d8993
Joseph Musya <josephmusya254@gmail.com>
2023-01-11 13:20:54 +0300

    Added more styling and components

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -21,14 +21,39 @@ function MainPage(){
                         </div>
                     </div>
                     <div className={styles.controls}>
-                        <strong>Quantity:&nbsp;<span>30 Litres</span></strong>
+                        <strong>Quantity:&nbsp;<span>300 Litres</span></strong>
                         <button className="btn text-white bg-primary">Close</button>
                         <button className='btn text-white bg-danger'>Open</button>                        
                     </div> 
-                    <Link to='/tank-setup' className='badge bg-danger btn'>Setup</Link>
-                </div>
-                               
-            </div>                            
+                    <Link to='/tank-setup' className='badge bg-danger btn fs-5 rounded-pill'>Setup</Link>
+                </div>                               
+            </div>  
+            <hr /> 
+            <div className={styles.tankElement}>
+                <div>
+                    <h2>Farm Tank</h2>
+                    <div id="tank" className={styles.tank}>
+                    <div id="water-level" className={styles.waterlevel}></div>
+                        <div className={styles.wave}>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                            <div className={styles.box}></div>
+                        </div>
+                    </div>
+                    <div className={styles.controls}>
+                        <strong>Quantity:&nbsp;<span>1000 Litres</span></strong>
+                        <button className="btn text-white bg-primary">Close</button>
+                        <button className='btn text-white bg-danger'>Open</button>                        
+                    </div> 
+                    <Link to='/tank-setup' className='badge bg-danger btn fs-5 rounded-pill'>Setup</Link>
+                </div>                               
+            </div>                         
         </div>
     
     );
