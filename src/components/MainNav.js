5461b40e8088beb509dee56e0ff6d097776ab248
Joseph Musya <josephmusya254@gmail.com>
2023-01-11 12:07:10 +0300

    Added more nav elements

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,13 +1,18 @@
 import {Link} from 'react-router-dom'
+import Logo from './media/logo.webp'
 function Navbar(){
     return (
         <nav>
-            <div>
-                <strong>Waziup Water Management</strong>
+            <div className='logo'>
+                <img src={Logo} alt="Waziup Logo" />
+            </div>
+            <div>                
+                <strong>Waziup Water Management</strong>                
             </div>
             <div className="nav-elements">
                 <Link to='/'>Home</Link>
-                <Link to='/tank-setup'>Tank Setup</Link>
+                <Link to='/user-profile'>Profile</Link>
+                <Link to='/alerts'>Alerts</Link>
             </div>
         </nav>
     );
