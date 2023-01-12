e22cd2054b07363ab99ebcd073603246ee2c063f
Joseph Musya <josephmusya254@gmail.com>
2023-01-12 22:28:16 +0300

    Restricted displayed content

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,13 +1,25 @@
 import {Link} from 'react-router-dom'
 import Logo from './media/logo.webp'
-import {useNavigate} from 'react-router-dom';
+import {useContext} from 'react';
+import {LoginContext} from '../providers/LoginProvider';
 
-function Navbar(){
-    const navigate = useNavigate();
+function Navbar(){    
+    const [loginStatus, setLoginStatus] = useContext(LoginContext)
+
     function logoutHandler(){
         window.localStorage.removeItem('token')
-        console.log('Logged out..')
-        navigate('/')
+        setLoginStatus(false)
+    }
+
+    function displayNavItems(){
+        return(
+            <div className="nav-elements"> 
+                <Link to='/'>Home</Link>
+                <Link to='/user-profile'>Profile</Link>
+                <Link to='/alerts'>Alerts</Link>
+                <Link onClick={logoutHandler}>Logout</Link>               
+            </div>            
+        )
     }
     return (
         <nav>
@@ -16,13 +28,12 @@ function Navbar(){
             </div>
             <div>
                 <strong>Waziup Water Management</strong>                
-            </div>
-            <div className="nav-elements">
-                <Link to='/'>Home</Link>
-                <Link to='/user-profile'>Profile</Link>
-                <Link to='/alerts'>Alerts</Link>
-                <Link onClick={logoutHandler}>Logout</Link>
-            </div>
+            </div>            
+                {
+                    loginStatus ? displayNavItems():
+                    <div></div>                    
+                }
+            
         </nav>
     );
     
