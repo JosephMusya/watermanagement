d0f337ae826f6d022fd65a98eb80a3be92c99f8e
Joseph Musya <josephmusya254@gmail.com>
2023-01-15 21:05:39 +0300

    Removed headers from the fetch API request

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -12,17 +12,7 @@ function MainPage(){
         async function getDevices(token){
         console.log('---------------------')
         const res = await fetch(
-            'https://api.waziup.io/api/v2/devices?q=owner==muciajoe@gmail.com',
-            {
-                // mode:'cors',
-                // headers: new Headers({
-                //     'Accept':'application/json;charset=utf-8',
-                //     'Authorization':`Bearer: ${token}`
-                // })
-                // headers:{
-                //     'Authorization': `Bearer ${token}`,
-                // }
-            }
+            'https://api.waziup.io/api/v2/devices?q=owner==muciajoe@gmail.com',           
         );
         const data = await res.json();
         setLoading(false)
@@ -35,39 +25,9 @@ function MainPage(){
 
     getDevices(token)
 
-    },[])
-
-
-    
-    // var reconnectTimeout = 2000;
-    // var mqtt = new window['Paho'].MQTT.Client("api.waziup.io", Number(443), "/websocket", "clientjs");
-    // var options = {
-    //     useSSL: true,
-    //     timeout: 5,
-    //     onSuccess: onConnect,
-    //     onFailure: onFailure};
-
-    // mqtt.connect(options)     
-    // mqtt.onMessageArrived = onMessageArrived;
-
-
-    // function onConnect() {
-    //     console.log("Connected to MQTT");
-    //     mqtt.subscribe("devices/MyDevice/sensors/TC1/value");
-    //   }
-
-    // function onFailure(message) {
-    //     console.log("Failed", message);
-    //     setTimeout(window['MQTTconnect'], reconnectTimeout);
-    // }
-
-    // function onMessageArrived(msg) {
-    //     console.log(msg.payloadString);
-    //         // "New value: " + JSON.parse(msg.payloadString).value + " °C";
-    // }
+    },[])        
     
     return (
-        // token?<Tank devices={(getDevices(token))}/>:<Login/>
         token?(
             loading?<div>Loading...</div>:<Tank devices={loadedData}/>
         ):<Login/>
