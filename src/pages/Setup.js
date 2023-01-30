57027f889e7b9695d5fcd69bd1d78f93d5451059
Joseph Musya <josephmusya254@gmail.com>
2023-01-30 13:45:31 +0300

    Added event change handler

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -1,37 +1,49 @@
 import styles from './Setup.module.css';
 function TankSetup(){
+    const maxLevelHandler = (e) => {
+        console.log("Max Level: ",e.target.value);
+    }
+
+    const minLevelHandler = (e) => {
+        console.log("Min Level: ",e.target.value)
+    }
+
+    const maxAlarmHandler  = (e) => {
+        console.log("Alarm Max: ",e.target.value)
+    }
+    const minAlarmHandler = (e) => {
+        console.log("Alarm Max: ", e.target.value)
+    }
     return(
         <div className={styles.main}>
             <div className={styles.setup}>
-                <h2>Tank Setup</h2>
-                <strong htmlFor="Tank-Type">Tank Type</strong>
-                <select className="form-control" name="Tank" id="tank-type">
-                    <option value="Cylindrical">Cylindrical</option>
-                    <option value="Cubic">Cubic</option>
-                </select>
+                <h2>Tank Setup</h2>                
                 <h3>Capacity</h3>
                 <form className={styles.form}>
                     <div>
                         <label forhtml="max-level">Max Level</label>
-                        <input className="form-control" type="number" min="0" value="1000"/>
+                        <input className="form-control" type="number" min="0" defaultValue={1000} onChange={maxLevelHandler}/>
                         <label htmlFor="max-level">Lts</label>
                     </div>
                     <div>
-                        <label htmlFor="max-level">Min Level</label>
-                        <input className="form-control" type="number" min="0" value="0"/>
+                        <label htmlFor="min-level">Min Level</label>
+                        <input className="form-control" type="number" min="0" defaultValue={0} onChange={minLevelHandler}/>
                         <label htmlFor="max-level">Lts</label>
                     </div>
                 <hr/>
                 <h3>Alarms</h3>
                     <div>
                         <label htmlFor="max-level">Max Level</label>
-                        <input className="form-control" type="number" min="0" value="900"/>
+                        <input className="form-control" type="number" min="0" defaultValue={1000} onChange={maxAlarmHandler}/>
                         <label htmlFor="max-level">Lts</label>
                     </div>
                     <div>
                         <label htmlFor="max-level">Min Level</label>
-                        <input className="form-control" type="number" min="0" value="100"/>
+                        <input className="form-control" type="number" min="0" defaultValue={10} onChange={minAlarmHandler}/>
                         <label htmlFor="max-level">Lts</label>
+                    </div>
+                    <div className={styles.submit}>
+                        <button>Complete Setup</button>
                     </div>
                 </form>
             </div>
